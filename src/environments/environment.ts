57acf55dcfadbfda9f4e1d4565b44daf2c26dd1a
Vaibhavsandbho <vaibhav.sandbhor@ats-group.com>
2026-06-27 23:23:13 +0530

    s04

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,9 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api'
+  // apiUrl: 'http://localhost:8080/api'
+
+  // apiUrl:'http://localhost:8080/rent-management-api-0.0.1-SNAPSHOT/api'
+
+
+  apiUrl:'http://10.227.250.161:8085/rent-management-api-0.0.1-SNAPSHOT/api'
 };
